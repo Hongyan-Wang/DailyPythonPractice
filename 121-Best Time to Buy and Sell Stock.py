68026d49a0752841be6da53eb3c9a4659cfbf4c4
Yvonne Wang <hongyan.wang118@gmail.com>
2024-09-09 23:46:39 +0100

    Update 121-Best Time to Buy and Sell Stock.py

--- 121-Best Time to Buy and Sell Stock.py
+++ 121-Best Time to Buy and Sell Stock.py
@@ -1,14 +1,16 @@
+### https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
+
 class Solution(object):
+    '''
     def maxProfit1(self, prices):
-        # 超时算法
         res = 0
         for i in range(len(prices)):
             for j in range(i, len(prices)):
                 res = max(prices[j] - prices[i], res)
         return res
+    '''
 
     def maxProfit(self, prices):
-        # 零维动态规划
         if not prices: return 0
         dp = prices[0]
         res = 0
